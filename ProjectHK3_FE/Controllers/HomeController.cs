09049289d6f0aba77dddf5ad68a88a391827a73d
tranlocccc123 <ttranloc2004@gmail.com>
2024-04-21 00:38:31 +0700

    đăng ký đăng nhập logout

--- ProjectHK3_FE/Controllers/HomeController.cs
+++ ProjectHK3_FE/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ProjectHK3_FE.Models;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ProjectHK3_FE.Controllers
 {
@@ -13,9 +16,41 @@ namespace ProjectHK3_FE.Controllers
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+        public async Task<ActionResult> Index()
 		{
-			return View();
+			// Khởi tạo HttpClient
+			using (HttpClient client = new HttpClient())
+            {
+                //                                @RenderSection("ProductList", required: false)
+
+                // Địa chỉ của API bạn muốn gọi
+                string apiUrl = "https://localhost:7283/api/TaiKhoanMatKhau/GetTaiKhoanMatKhau";
+                //images/women-clothes-img.png
+
+                // Gọi API và nhận dữ liệu trả về
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Đọc nội dung JSON từ response
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    List<Product> productList = JsonConvert.DeserializeObject<List<Product>>(responseData);
+
+                    // Truyền dữ liệu nhận được từ API vào ViewBag
+                    //ViewBag.ApiData = responseData;
+                    return View(productList);
+
+                }
+                else
+                {
+                    // Xử lý lỗi nếu có
+                    //ViewBag.ApiData = "Error calling the API";
+                    return View(new List<Product>());
+
+                }
+            }
+
 		}
 
 		public IActionResult Privacy()
---
+++ ProjectHK3_FE/Controllers/UserController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ProjectHK3_FE.Models;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProjectHK3_FE.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View(new List<Product>());
+		}
+		public IActionResult Register()
+		{
+			return View(new List<Product>());
+		}
+
+        public async Task<IActionResult> Logout()
+        {
+            using (var client = new HttpClient())
+            {
+                // Đặt URL của API
+                string apiUrl = "https://localhost:7283/api/Auth/logout";
+
+                //var json = $"{{\"username\":\"{username}\", \"password\":\"{password}\"}}";
+                //var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(apiUrl, null);
+
+                // Xác định liệu request có thành công không
+                if (response.IsSuccessStatusCode)
+                {
+                    // Đọc và parse dữ liệu JSON từ response
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    HttpContext.Session.Remove("Username");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return Content("Error: " + response.StatusCode.ToString());
+                }
+            }
+        }
+
+        [HttpPost]
+		public async Task<ActionResult> SendLogin(string username, string password)
+		{
+			// Gọi API và nhận dữ liệu trả về
+			using (var client = new HttpClient())
+			{
+				// Đặt URL của API
+				string apiUrl = "https://localhost:7283/api/Auth/login";
+
+				var json = $"{{\"username\":\"{username}\", \"password\":\"{password}\"}}";
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+				//client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+				// Gửi POST request đến API và nhận dữ liệu JSON
+				HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+				// Xác định liệu request có thành công không
+				if (response.IsSuccessStatusCode)
+				{
+					// Đọc và parse dữ liệu JSON từ response
+					string responseData = await response.Content.ReadAsStringAsync();
+
+					HttpContext.Session.SetString("Username", username);
+                    TempData["LoginSuccess"] = true;
+
+                    return RedirectToAction("Index", "Home");
+                    //return Content(responseData, "application/json");
+					
+				}
+				else
+				{
+					// Trả về lỗi nếu request không thành công
+					return Content("Error: " + response.StatusCode.ToString() + " uname " + username + "  pass " + password);
+				}
+			}
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> SendRegister(string username, string password)
+		{
+			// Gọi API và nhận dữ liệu trả về
+			using (var client = new HttpClient())
+			{
+				// Đặt URL của API
+				string apiUrl = "https://localhost:7283/api/Auth/register";
+
+				var json = $"{{\"username\":\"{username}\", \"password\":\"{password}\"}}";
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+				//client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+				// Gửi POST request đến API và nhận dữ liệu JSON
+				HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+				// Xác định liệu request có thành công không
+				if (response.IsSuccessStatusCode)
+				{
+					// Đọc và parse dữ liệu JSON từ response
+					string responseData = await response.Content.ReadAsStringAsync();
+
+					HttpContext.Session.SetString("Username", username);
+
+					return RedirectToAction("Index", "Home");
+
+				}
+				else
+				{
+					return Content("Error: " + response.StatusCode.ToString() + " uname " + username + "  pass " + password);
+				}
+			}
+		}
+
+    }
+}
